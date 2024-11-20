094eb99d8cd76de8a53420aeb51244ed950480a0
Liam Hayes <liamh@scc-201.scc.bu.edu>
2024-11-20 13:51:02 -0500

    dep cheetah init

---
+++ dep_cheetah.py
@@ -0,0 +1,27 @@
+"""
+Pure DEP control of the cheetah
+"""
+
+from dm_control import suite
+import numpy as np
+
+class DEP_controller:
+    ## TODO ##
+    pass
+
+# Load environment
+env = suite.load(domain_name="cheetah", task_name="run")
+
+# Do simulation
+action_spec = env.action_spec()
+time_step = env.reset()
+frames = []
+while not time_step.last():
+    # Get action and do it
+    action = None ## TODO ## Get DEP action
+    time_step = env.step(action)
+
+    # Render and capture frame
+    frame = env.physics.render()
+    frames.append(frame)
+
