b860d344f34fad434186db35e775f9886a66e845
Liam Hayes <liamh@scc-201.scc.bu.edu>
2024-11-20 13:50:43 -0500

    utils file init

---
+++ utils.py
@@ -0,0 +1,22 @@
+"""
+Utility functions
+"""
+
+import cv2
+import numpy as np
+
+
+# Visualize the episode
+def make_video(frames, video_name):
+    height, width, layers = frames[0].shape
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_name = f'videos/{video_name}.avi'
+    fps = 30
+
+    out = cv2.VideoWriter(video_name, fourcc, fps, (width, height))
+
+    for frame in frames:
+        out.write(frame)
+
+    out.release()
+    cv2.destroyAllWindows()
