c1d26f86609e81dcc5d2b1ede2c56e4b9927efe2
LiamCHayes <liamcharleshayes@gmail.com>
2024-11-28 06:39:02 -0500

    learning to imitate DEP

--- dep_imitation.py
+++ dep_imitation.py
@@ -22,35 +22,49 @@ env = suite.load(domain_name="cheetah", task_name="run")
 action_size = env.action_spec().shape[0]
 observation_size = env.observation_spec()['position'].shape[0]
 
-model = DEPActor(observation_size, action_size, 1e-3)
-
-# Initialize lists to track DEP
-frames = []
+actor = DEPActor(observation_size, action_size, 1e-3)
 
 # Do simulation
+num_episodes = 1001
 num_steps = 500
 time_step = env.reset()
+episode_loss = []
+for e in range(num_episodes):
+    print('\nEpisode ', e)
 
-for t in tqdm(range(num_steps)):
-    # Get action and do it
-    observation = time_step.observation['position']
-    observation_tensor = torch.tensor(observation, dtype=torch.float32).unsqueeze(0).to(device)
-    action = model.forward(observation_tensor)
-    action = action.cpu().detach().numpy()
-    time_step = env.step(action)
+    time_step = env.reset()
+    actor.reset()
+    total_loss = 0
+    frames = []
+    for t in tqdm(range(num_steps)):
+        # Get action and do it
+        observation = time_step.observation['position']
+        observation_tensor = torch.tensor(observation, dtype=torch.float32).unsqueeze(0).to(device)
+        action = actor.forward(observation_tensor)
+        action = action.cpu().detach().numpy()
+        time_step = env.step(action)
 
-    if reporting:
-        # Adjust camera positon
-        agent_pos = env.physics.named.data.xpos['torso']
-        env.physics.named.data.cam_xpos['side'][0] = agent_pos[0]
+        total_loss += actor.im_loss.item()
+
+        if e % 10 == 0:
+            # Adjust camera positon
+            agent_pos = env.physics.named.data.xpos['torso']
+            env.physics.named.data.cam_xpos['side'][0] = agent_pos[0]
+            
+            # Render frame and save
+            frame = env.physics.render(camera_id = 'side')
+            frames.append(frame)
         
-        # Render frame and save
-        frame = env.physics.render(camera_id = 'side')
-        frames.append(frame)
-    
-    # Break if episode is over
-    if time_step.last():
-        break
+        # Break if episode is over
+        if time_step.last():
+            break
 
-# Visualize
-see_live(frames)
+    episode_loss.append(total_loss)
+    print('Loss: ', total_loss)
+
+    if e % 10 == 0:
+        # Visualize
+        see_live(frames)
+
+        # Save model
+        torch.save(actor.load_state_dict(), 'deep_dep_results/imitation.pth')
